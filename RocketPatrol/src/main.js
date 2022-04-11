3e96f0471116b4b844c59e131b01dc6169573648
“Thrisha <tkopula@ucsc.edu>
2022-04-11 12:18:22 -0700

    Created Rocket.js

--- RocketPatrol/src/main.js
+++ RocketPatrol/src/main.js
@@ -1,1 +1,17 @@
-let game = new Phaser.Game();
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480,
+    scene: [ Menu, Play ]
+  }
+let game = new Phaser.Game(config);
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
+
+// reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
+// define keys
+keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
