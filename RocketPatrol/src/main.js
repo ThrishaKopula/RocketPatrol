d459ec9ee422b68271b53fa0d57f144bdba6600a
“Thrisha <tkopula@ucsc.edu>
2022-04-11 12:41:25 -0700

    Finished game

--- RocketPatrol/src/main.js
+++ RocketPatrol/src/main.js
@@ -3,15 +3,13 @@ let config = {
     width: 640,
     height: 480,
     scene: [ Menu, Play ]
-  }
+}
+
 let game = new Phaser.Game(config);
+
+// set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
 
-// reserve keyboard vars
+// reserve keyboard variables
 let keyF, keyR, keyLEFT, keyRIGHT;
-// define keys
-keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
-keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
-keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
